2f27b26d7faa4d08e8ceb3217f47c329e8aba51c
dylanToews <dylan.toews@yahoo.ca>
2022-11-21 14:07:51 -0700

    route refactored for ind-quiz

--- routes/ind-quiz.js
+++ routes/ind-quiz.js
@@ -1,31 +1,17 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const quizQueries = require('../db/queries/queries');
-
-// router.get('/quizzes', (req, res) => {
-//   quizQueries.getOneQuiz()
-//     .then(users => {
-//       res.json({ users });
-//       console.log('getOneQuiz function called')
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-// });
-
 
 
 router.get('/quiz/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   quizQueries.getQuizFromId(id)
     .then(result => {
       const templateVars = {
         quiz: result
-      }
-      console.log('quiz templatevars', templateVars)
-      res.render('quiz', templateVars)
+      };
+      console.log('quiz templatevars', templateVars);
+      res.render('quiz', templateVars);
     })
     .catch(err => {
       res
