cbef10df1aa424ad871f421e671e5302f4eeb15d
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-11-21 13:30:01 -0500

    inserting data into tables

---
+++ db/queries/createTable.sql
@@ -0,0 +1,28 @@
+const db = require('../connection');
+
+const creatQuiz = () => {
+  const queryString = `
+  INSERT INTO quizzes (
+    title,
+    description,
+    private,
+    user_quiz_id,
+  ) VALUSE ( $1, $2, $3, $4) 
+  RETURNING*;
+ *** We should know the generated id to insert into question tables ***
+  INSERT INTO questions ( 
+    question_content,
+    correct_answer,
+    wrong_answer1,
+    wrong_answer2,
+    wrong_answer3
+    ) VALUES ($5, $6, $7, $8, $9)
+    RETURNING*;
+    ` 
+  return pool.query( queryString , Object.values(quiz))
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { creatQuiz };
