43bb429969995dae95bf481b97ee18fe8548f5ac
dylanToews <dylan.toews@yahoo.ca>
2022-11-22 10:32:04 -0700

    radio input (multiple choice questions) on quiz.ejs fixed so the correct answer is linked to database and also correctly varified

--- routes/create-quiz.js
+++ routes/create-quiz.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const quizQueries = require('../db/queries/queries');
 
 //Render Create Quiz page
 
 router.get('/create', (req, res) => {
   console.log('create quiz');
-  //res.send('create quiz');
   res.render('create');
 });
+
+router.post('/create', (req, res) => {
+
+
+
+})
 
 
 
