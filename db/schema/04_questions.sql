16ffd18282cfec04f8d3a65eea4cfa2f7bd398d1
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-11-24 16:36:14 -0500

    no change

--- db/schema/04_questions.sql
+++ db/schema/04_questions.sql
@@ -3,11 +3,11 @@
 DROP TABLE IF EXISTS questions CASCADE;
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
-  question_content VARCHAR(255) NOT NULL,
-  correct_answer VARCHAR(255) NOT NULL,
-  incorrect_1 VARCHAR(225) NOT NULL,
-  incorrect_2 VARCHAR(255) NOT NULL,
-  incorrect_3 VARCHAR(225) NOT NULL,
+  question_content VARCHAR(255),
+  correct_answer VARCHAR(255),
+  incorrect_1 VARCHAR(225),
+  incorrect_2 VARCHAR(255),
+  incorrect_3 VARCHAR(225),
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE
 );
 
