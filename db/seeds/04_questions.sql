8b7c59d25f6a4e0918186df76a0e6fe5f28a7777
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-11-24 15:45:40 -0500

    last change

--- db/seeds/04_questions.sql
+++ db/seeds/04_questions.sql
@@ -1,18 +1,30 @@
 -- -- questions table seeds --
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('In what year did the first episode of The Simpsons air? ?', '1989', '1980', '1995', '1998', 1);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('Which cartoon inspired The Itchy & Scratchy Show?', 'Tom and Jerry', 'Pink Panther', 'Pokemon', 'Pepepig', 1);
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('In what year did the first episode of The Simpsons air?', '1989', '1980', '1995', '1998', 1);
+
 INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('How many children to Apu and Manjula have? ?', ' Eight', 'Five', 'Seven', 'Three', 2);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Mr Burns catchphrase?', 'Excellent! ', 'Great', 'Bravo', 'Super!', 2);
+
 INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('Which member of the Simpsons family is a vegetarian?', 'Lisa Simpson', 'Bart Simpson', 'Maggie Simpson', 'Mona Simpson', 3);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What did Bart want for Christmas in the first episode of season one?', 'A Tattoo', 'A Skateboard', 'A Pair of Shoes', 'Toy', 3);
+
 INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Homer s favorite beer?', 'Duff Beer', 'Teddy Beer', 'Huffi Beer', 'White Beer', 4);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('Who is Homer s mother?', 'Mona', 'Lisa', 'Maggie', 'Marri', 4);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Homer s job?', 'Nuclear Safety Inspector', 'Teacher', 'Policeman', 'Postman', 4);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('When Homer became a baseball mascot, what was his name?', 'Dancing Homer', 'Hero', 'Speedy Homer', 'Super Homer', 4);
+
 INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What colour is the Simpsons iconic sofa?', 'Orange', 'Blue', 'Pink', 'Aquablue', 5);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is the name of Simpsons current cat?', 'Snowball', 'Tom', 'Scotti', 'Soriball', 5);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('In which town do the Simpsons live?', 'Springfield', 'Santa Barbara', 'California', 'Nevada', 5);
+
 INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is the name of Marge s inmate when she was in prison?', 'Phillips', 'Gerrard', 'Mike', 'Paul', 6);
-INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Marge s natural hair colour?', 'Grey', 'Yellow', 'Blue', 'Brown', 6);
 
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('Which cartoon inspired The Itchy & Scratchy Show?', 'Tom and Jerry', 'Pink Panther', 'Pokemon', 'Pepepig', 7);
 
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Mr Burns catchphrase?', 'Excellent! ', 'Great', 'Bravo', 'Super!', 8);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What did Bart want for Christmas in the first episode of season one?', 'A Tattoo', 'A Skateboard', 'A Pair of Shoes', 'Toy', 9);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('Who is Homer s mother?', 'Mona', 'Lisa', 'Maggie', 'Marri', 10);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Homer s job?', 'Nuclear Safety Inspector', 'Teacher', 'Policeman', 'Postman', 11);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('When Homer became a baseball mascot, what was his name?', 'Dancing Homer', 'Hero', 'Speedy Homer', 'Super Homer', 12);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is Marge s natural hair colour?', 'Grey', 'Yellow', 'Blue', 'Brown', 13);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('What is the name of Simpsons current cat?', 'Snowball', 'Tom', 'Scotti', 'Soriball', 14);
+
+INSERT INTO questions (question_content, correct_answer, incorrect_1, incorrect_2, incorrect_3, quiz_id) VALUES ('In which town do the Simpsons live?', 'Springfield', 'Santa Barbara', 'California', 'Nevada', 15);
